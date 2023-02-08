dd7bb90420948590658b8619566f9813a3870458
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-08 17:34:56 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/products/:id",
+    element: <div>Category</div>,
+  },
+  {
+    path: "/product/:id",
+    element: <div>Product</div>,
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
