c4df9af969b69a99a5580833a9539c76becac760
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-09 02:13:37 +0100

    Create Slider.jsx and other components and pages
    Add images and translate animation to Slider.jsx

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./FeaturedProducts.scss"
+
+function FeaturedProducts() {
+  return (
+    <div className='featured-products'>FeaturedProducts</div>
+  )
+}
+
+export default FeaturedProducts
