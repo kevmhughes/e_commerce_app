e31116b3892420645ca1ea89c836a865e6fc5f32
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-09 01:21:06 +0100

    Create Navbar.jsx and Footer.jsx and style these common components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
+import React from "react";
+
 import Home from "./pages/Home/Home"
-import Products from "./pages/Products/Products"
 import Product from "./pages/Product/Product"
+import Products from "./pages/Products/Products"
+
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+import "./App.scss"
 
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
 
-
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products/:id",
-    element: <Products />,
-  },
-  {
-    path: "/product/:id",
-    element: <Product />,
+    element: <Layout />,
+    children: [
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/product/:id",
+        element: <Product/>
+      },
+      {
+        path:"/products/:id",
+        element: <Products/>
+      }
+    ],
   },
 ]);
 
