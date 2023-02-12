9f79977a951eab639d49f5ec1ecad92a49e9d7d7
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-12 03:24:05 +0100

    Create .env and update Card.jsx and FeaturedProducts.jsx components
    Connect app to Strapi

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,48 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Card/Card'
 import "./FeaturedProducts.scss"
+import axios from "axios"
 
 const FeaturedProducts = ({type}) => {
 
-    const data = [
-        {
-            id: 1,
-            img: "https://images.pexels.com/photos/9741917/pexels-photo-9741917.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            img2: "https://images.pexels.com/photos/9741911/pexels-photo-9741911.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            title: "spiral t-shirt",
-            isNew:true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 2,
-            img: "https://images.pexels.com/photos/9558231/pexels-photo-9558231.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            img2: "https://images.pexels.com/photos/9558244/pexels-photo-9558244.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            title: "long red shirt dress",
-            isNew:true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 3,
-            img: "https://images.pexels.com/photos/13833041/pexels-photo-13833041.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            img2: "https://images.pexels.com/photos/13833042/pexels-photo-13833042.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            title: "NASA t-shirt",
-            isNew:true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 4,
-            img: "https://images.pexels.com/photos/8973512/pexels-photo-8973512.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            img2: "https://images.pexels.com/photos/8973515/pexels-photo-8973515.jpeg?auto=compress&cs=tinysrgb&w=1200",
-            title: "Lakers slacks",
-            isNew:true,
-            oldPrice: 19,
-            price: 12,
-        },
-    ];
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const res = await axios.get(
+                    process.env.REACT_APP_API_URL + "/products?populate=*", 
+                    {
+                headers: {
+                    Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+                } 
+                });
+                setData(res.data.data)
+            } catch (err) {
+                console.log(err)
+            }
+        };
+        fetchData();
+    }, [])
+
+    console.log(data)
 
     console.log(data.map(item => item.title))
   return (
