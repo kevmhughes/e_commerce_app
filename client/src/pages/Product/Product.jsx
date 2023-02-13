c17fc10994bd2a6b7112f8db134a05053c3c581d
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-13 02:27:46 +0100

    Update List.jsx, Products.jsx and Product.jsx

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -3,39 +3,47 @@ import { useState } from 'react'
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BalanceIcon from '@mui/icons-material/Balance';
+import { useParams } from 'react-router-dom';
+import useFetch from "../../hooks/useFetch";
 import "./Product.scss"
+
 
 const Product = () => {
 
-  const [selectedImg, setSelectedImg] = useState(0)
-  const [quantity, setQuantity] = useState(1)
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
+  const [quantity, setQuantity] = useState(1);
 
-  const images = [
-    "https://images.pexels.com/photos/336372/pexels-photo-336372.jpeg?auto=compress&cs=tinysrgb&w=1200",
-    "https://images.pexels.com/photos/845434/pexels-photo-845434.jpeg?auto=compress&cs=tinysrgb&w=1200"
-
-  ]
+  const {data, loading, error} = useFetch(
+    `/products/${id}?populate=*`
+    );
 
   return (
     <div className='product'>
-      <div className="left">
+      {loading ? (
+        "loading") :(
+        <>
+        <div className="left">
         <div className="images">
-          <img src={images[0]} alt="fashion" onClick={e => setSelectedImg(0)}/>
-          <img src={images[1]} alt="fashion" onClick={e => setSelectedImg(1)} />
+          <img 
+          src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url} 
+          alt="fashion" 
+          onClick={e => setSelectedImg("img")}
+          />
+          <img 
+          src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url} 
+          alt="fashion" 
+          onClick={e => setSelectedImg("img2")} 
+          />
         </div>
         <div className="mainImg">
-          <img src={images[selectedImg]} alt="fashion"/>
+          <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg].data?.attributes?.url} alt="fashion"/>
         </div>
       </div>
       <div className="right">
-        <h1>Title</h1>
-        <span className='price'>$199</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-          culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h1>{data?.attributes?.title}</h1>
+        <span className='price'>${data?.attributes?.price}</span>
+        <p>{data?.attributes?.desc}</p>
           <div className="quantity">
             <button onClick={() => setQuantity(prev => (prev === 1 ? 1 : prev - 1))}>-</button>
             {quantity}
@@ -66,8 +74,10 @@ const Product = () => {
             <span>FAQ</span>
           </div>
       </div>
+      </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Product
