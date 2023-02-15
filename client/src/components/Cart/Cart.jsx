a9cd7658ea2026d86cab2b9e292304234009616a
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-15 02:57:35 +0100

    Create order.js and update Cart.jsx and store.js
    Add Stripe payment processing functionality

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,6 +3,8 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import "./Cart.scss"
 import { useDispatch, useSelector } from 'react-redux';
 import { removeItem, resetCart } from '../../redux/cartReducer';
+import {loadStripe} from '@stripe/stripe-js';
+import { makeRequest } from "../../makeRequest.js"
 
 const Cart = () => {
     // state.cart reaches store, and then state.cart.products reaches products in cartReducer 
@@ -13,6 +15,24 @@ const  totalPrice = () => {
     let total = 0;
     products.forEach((item) => (total += item.quantity * item.price));
     return total.toFixed(2);
+};
+
+const stripePromise = loadStripe('pk_test_51MbYZKLpZald5BFxagLlq4yi1EdJOxOlyLFas4uHDTcOfm243BKAeMND7ibqf7Iz6DmkalY5fLcXby6mPBdbX0Y400bITnyIWk');
+
+const handlePayment = async () => {
+    try {
+        const stripe = await stripePromise;
+
+        const res = await makeRequest.post("/orders", {
+            products
+        });
+
+        await stripe.redirectToCheckout({
+            sessionId: res.data.stripeSession.id, 
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 
@@ -35,7 +55,7 @@ const  totalPrice = () => {
             <span>SUBTOTAL</span>
             <span>${totalPrice()}</span>
         </div>
-        <button>PROCEED TO CHECKOUT</button>
+        <button onClick={handlePayment} >PROCEED TO CHECKOUT</button>
         <span className='reset' onClick={() => dispatch(resetCart())}>Reset Cart</span>
     </div>
   )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -12,6 +12,8 @@ import {
   REGISTER,
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+const stripe = require('stripe')('sk_test_51MbYZKLpZald5BFxRTF2cA5eOrnD5MvgV1Nmv5BnBJ9S30YoDfZJ4KOrdSCf7AvgvxsyCp1N2sNlSONhBz0TGnYT00tETKQEGI');
+
 
 const persistConfig = {
   key: 'root',
